d4e60b4ba88257660c4b6eacc0c6117ed0dbf93c
A <alexanderkerschen@gmail.com>
2025-01-22 19:28:33 -0600

    refactor render_update

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,18 +3,15 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour
 {
 
-    public GlobalOperations global;
     public GridManager grid_manager;
     public Toolbox toolbox;
     private ControlFile controls;
     private bool clicked_grid; 
-    private (int row, int col) prev_grid_pos; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         clicked_grid = false;
-        prev_grid_pos = (-1, -1);
     }
 
     void Awake(){
@@ -30,19 +27,7 @@ public class InputHandler : MonoBehaviour
     void Update()
     {
         if (controls.Grid.MainClick.IsPressed()){
-            Vector3 mouse_pos = Input.mousePosition;
-            (int row, int col) grid_pos = grid_manager.get_grid_pos(mouse_pos);
-
-
-            if (prev_grid_pos != grid_pos){
-                toolbox.tool_draw(grid_pos, prev_grid_pos);
-            }
-
-
-            if (prev_grid_pos != grid_pos){
-                global.render_update = true;
-            }
-            prev_grid_pos = grid_pos;
+            toolbox.tool_draw();
             clicked_grid = true;
         }
         else if (clicked_grid && !controls.Grid.MainClick.IsPressed()){
--- Assets/Scripts/Toolbox.cs
+++ Assets/Scripts/Toolbox.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class Toolbox : MonoBehaviour
 {
 
+    public GlobalOperations global;
     public GridManager grid_manager;
     public char active_tool;
     public char active_letter;
+    private (int row, int col) prev_grid_pos; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         active_tool = 'p';        
         active_letter = 'a';
+        prev_grid_pos = (-1, -1);
     }
 
     // Update is called once per frame
@@ -20,15 +23,24 @@ public class Toolbox : MonoBehaviour
         
     }
 
-    public void tool_draw((int row, int col) mouse_pos, (int row, int col) prev_mouse_pos){
+    public void tool_draw(){
+        Vector3 mouse_pos = Input.mousePosition;
+        (int row, int col) grid_pos = grid_manager.get_grid_pos(mouse_pos);
+
+
         if (active_tool == 'p'){
-            pencil(mouse_pos, prev_mouse_pos);
+            pencil(grid_pos, prev_grid_pos);
         }
+
+        if (prev_grid_pos != grid_pos){
+            global.render_update = true;
+        }
+        prev_grid_pos = grid_pos;
     }
 
-    private void pencil((int row, int col) mouse_pos, (int row, int col) prev_grid_pos){
-        if (mouse_pos != prev_grid_pos){
-            grid_manager.add_to_preview_buffer(mouse_pos.row, mouse_pos.col, active_letter);
+    private void pencil((int row, int col) grid_pos, (int row, int col) prev_grid_pos){
+        if (grid_pos != prev_grid_pos){
+            grid_manager.add_to_preview_buffer(grid_pos.row, grid_pos.col, active_letter);
         }
     }
 
