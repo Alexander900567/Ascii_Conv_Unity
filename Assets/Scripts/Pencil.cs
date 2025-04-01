4aa6ed758a24c731062511b18422e09ad0f8b39b
FyreAxis <xxcomedyskitzxx@gmail.com>
2025-04-01 15:32:45 -0500

    Adds prototype for pencil stroke.

--- Assets/Scripts/Pencil.cs
+++ Assets/Scripts/Pencil.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 
 public class Pencil : Tool
 {
-
+    [SerializeField] private Line Line;
     private (int row, int col) prevGpos;
 
-    public override void draw(){
-        (int row, int col) gpos = gridManager.getGridPos();
+        public override void draw(){
+            (int row, int col) gpos = gridManager.getGridPos();
 
-        if (gpos != prevGpos){
-            gridManager.addToPreviewBuffer(gpos.row, gpos.col, globalOperations.activeLetter);
-        } 
+            if (gpos != prevGpos){
+                gridManager.addToPreviewBuffer(gpos.row, gpos.col, globalOperations.activeLetter);
 
-        prevGpos = gpos;
+                (int row, int col) topLeft = gpos;
+                (int row, int col) bottomRight = gpos;
+
+                for (int i = 0; i <= Toolbox.GetStrokeWidth(); i++) {
+                    Line.line(
+                        (i, topLeft.col),
+                        (i, bottomRight.col),
+                        false
+                    );
+                }   
+            prevGpos = gpos;
+        }
     }
-
 }
